adb7480903d852e27125d209ed985cd50a9aea8a
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-02-01 00:37:31 +0530

    Add the databse config

---
+++ backend/VehicleTrackingService/Data/VehicleDbContext.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using VehicleTrackingService.Models;
+
+namespace VehicleTrackingService.Data
+{
+    public class VehicleDbContext : DbContext
+    {
+        public VehicleDbContext(DbContextOptions<VehicleDbContext> options) : base(options) {}
+
+        // public DbSet<VehicleLocation> VehicleLocations { get; set; }
+        public DbSet<BusLineStatus> VehicleLocations { get; set; }
+        public DbSet<LineStatus> LineStatuses { get; set; }
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+        public DbSet<Disruption> Disruptions { get; set; }
+        public DbSet<ValidityPeriod> ValidityPeriods { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+             modelBuilder.Entity<Disruption>(entity =>
+            {
+                entity.HasKey(e => e.id);
+                entity.Property(e => e.id).ValueGeneratedNever();
+
+                // Use value converter to store List<string> as a comma-separated string
+                entity.Property(e => e.affectedRoutes)
+                    .HasConversion(
+                        v => string.Join(",", v),  // Convert List<string> to comma-separated string
+                        v => v.Split(",", StringSplitOptions.RemoveEmptyEntries).ToList());  // Convert comma-separated string back to List<string>
+
+                entity.Property(e => e.affectedStops)
+                    .HasConversion(
+                        v => string.Join(",", v),  // Convert List<string> to comma-separated string
+                        v => v.Split(",", StringSplitOptions.RemoveEmptyEntries).ToList());  // Convert comma-separated string back to List<string>
+            });
+        }
+    }
+}
