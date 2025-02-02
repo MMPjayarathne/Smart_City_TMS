60b86e450bbfd197743fdc9a282bf6f0e05b2947
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-02-02 13:29:47 +0530

    Initiate the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import KafkaWebSocket from "./components/KafkaWebSocket";
+
+
+function App() {
+    return (
+        <div>
+            <h1>Kafka WebSocket Client</h1>
+            <KafkaWebSocket></KafkaWebSocket>
+        </div>
+    );
+}
+
+export default App;
