49e3b88df67c105c80df3c1fe00988f5e272e848
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-02-02 20:50:53 +0530

    create the main layout and add the sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import KafkaWebSocket from "./components/KafkaWebSocket";
-
+import Layout from "./components/main/Dashboard";
+import "./index.css";
+import "./src/styles/globals.css";
 
 function App() {
     return (
-        <div>
-            <h1>Kafka WebSocket Client</h1>
-            <KafkaWebSocket></KafkaWebSocket>
-        </div>
+    
+        <Layout>
+            <h1>Welcome to My App</h1>
+            <p>This content is inside the layout.</p>
+        </Layout>
+        
     );
 }
 
